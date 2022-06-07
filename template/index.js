e18e3455bdd146ea94ec2a81db2e977550d7efe4
George.Z <george.zhu@sjfood.com>
2022-06-07 15:01:19 +0800

    init

--- template/index.js
+++ template/index.js
@@ -2,8 +2,10 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+ import { AppRegistry } from 'react-native';
 
-AppRegistry.registerComponent(appName, () => App);
+ import { name as appName } from './app.json';
+ import App from './app/app';
+ 
+ AppRegistry.registerComponent(appName, () => App);
+ 
